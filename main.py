de2ff9c7b51cb1c96afe6d3179ef5eac593ac1ea
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2026-06-07 16:07:33 +0300

    body examples

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from fastapi import FastAPI
+
+
+from typing import Annotated
+
+from fastapi import Body, FastAPI
 from pydantic import BaseModel
+
+app = FastAPI()
 
 
 class Item(BaseModel):
@@ -9,13 +15,42 @@ class Item(BaseModel):
     tax: float | None = None
 
 
-app = FastAPI()
-
-
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.model_dump()
-    if item.tax is not None:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
+@app.put("/items/{item_id}")
+async def update_item(
+    *,
+    item_id: int,
+    item: Annotated[
+        Item,
+        Body(
+            openapi_examples={
+                "normal": {
+                    "summary": "A normal example",
+                    "description": "A **normal** item works correctly.",
+                    "value": {
+                        "name": "Foo",
+                        "description": "A very nice Item",
+                        "price": 35.4,
+                        "tax": 3.2,
+                    },
+                },
+                "converted": {
+                    "summary": "An example with converted data",
+                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+                    "value": {
+                        "name": "Bar",
+                        "price": "35.4",
+                    },
+                },
+                "invalid": {
+                    "summary": "Invalid data is rejected with an error",
+                    "value": {
+                        "name": "Baz",
+                        "price": "thirty five point four",
+                    },
+                },
+            },
+        ),
+    ],
+):
+    results = {"item_id": item_id, "item": item}
+    return results
